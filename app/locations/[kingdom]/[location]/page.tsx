2f137113fa39e1cfd3d9f941e9ee80f371f58259
youneshajizde <jonaselemented@gmail.com>
2026-08-12 15:38:52 +0330

    build ready

---
+++ app/locations/[kingdom]/[location]/page.tsx
@@ -0,0 +1,3 @@
+export default function LocationPage() {
+  return <div>Location page</div>;
+}
---
+++ app/weapons/[kingdom]/[weapon]/page.tsx
@@ -0,0 +1,7 @@
+export default function WeaponPage() {
+  return (
+    <div>
+      Weapon page
+    </div>
+  );
+}
